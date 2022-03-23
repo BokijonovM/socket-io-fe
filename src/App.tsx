798b92e8bf47af75f7eab2d4b375a0be0320404f
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-23 13:41:52 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-npm install react-bootstrap bootstrap@4.6.0
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyHome from "./components/MyHome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyHome />
     </div>
   );
 }
---
+++ src/components/MyHome.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyHome() {
+  return <div>MyHome</div>;
+}
+
+export default MyHome;
